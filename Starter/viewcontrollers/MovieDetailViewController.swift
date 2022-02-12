53b91f2251ead33d2305f35f9a2c7390a7b70bdc
Htet Lynn Htun <sainount2012@gmail.com>
2022-02-12 14:15:23 +0630

    Retry1 Section-2

--- Starter/viewcontrollers/MovieDetailViewController.swift
+++ Starter/viewcontrollers/MovieDetailViewController.swift
@@ -18,8 +18,8 @@ class MovieDetailViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-//        btnRateMovie.layer.borderColor = CGColor.init(red: 255, green: 255, blue: 255, alpha: 1)
-//        btnRateMovie.layer.borderWidth = 2
+        btnRateMovie.layer.borderColor = CGColor.init(red: 255, green: 255, blue: 255, alpha: 1)
+        btnRateMovie.layer.borderWidth = 2
 
         initGestureRecognizers()
 //        collectionViewActors.dataSource = self
