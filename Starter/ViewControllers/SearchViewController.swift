78c307dd476135fbf3e756275c84b5d2659b78fe
Htet Lynn Htun <25409428+HtetLynnHtun@users.noreply.github.com>
2022-05-07 09:44:59 +0630

    Remove old codes

--- Starter/ViewControllers/SearchViewController.swift
+++ Starter/ViewControllers/SearchViewController.swift
@@ -43,7 +43,6 @@ class SearchViewController: UIViewController, MovieItemDelegate {
     
     // MARK: - view setups
     private func setupSearchBar() {
-//        searchBar.delegate = self
         searchBar.placeholder = "Search..."
         searchBar.tintColor = .white
         let textOfSearchBar = searchBar.value(forKey: "searchField") as? UITextField
@@ -53,33 +52,9 @@ class SearchViewController: UIViewController, MovieItemDelegate {
     
     // MARK: - cell registrations
     private func registerCells() {
-//        collectionViewContent.dataSource = self
         collectionViewContent.delegate = self
         collectionViewContent.registerForCell(PopularFilmCollectionViewCell.identifier)
     }
-    
-    // MARK: - API methods
-    private func startSearching(query: String, page: Int) {
-        searchModel.searchMoviesAndSeries(query: query, page: page) { result in
-            switch result {
-            case .success(let searchResponse):
-                let data = searchResponse.results.filter { result in
-                    result.mediaType == "tv" || result.mediaType == "movie"
-                }
-                if (self.searchedQuery != query) {
-                    self.searchedQuery = query
-                    self.data = data
-                    self.totalPages = searchResponse.totalPages ?? 1
-                } else {
-                    self.data.append(contentsOf: data)
-                }
-                self.collectionViewContent.reloadData()
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
     
     // MARK: - onTap callbacks
     func onTapMovie(id: Int, contentType: DetailContentType) {
@@ -88,48 +63,7 @@ class SearchViewController: UIViewController, MovieItemDelegate {
     
 }
 
-// MARK: - ViewController extensions
-//extension SearchViewController: UISearchBarDelegate {
-//    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-//        searchBar.endEditing(true)
-//        if var query = searchBar.text {
-//            query = query.trimmingCharacters(in: .whitespacesAndNewlines)
-//            if !query.isEmpty {
-//                startSearching(query: query, page: 1)
-//            }
-//        }
-//    }
-//}
-//
 extension SearchViewController: UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        data.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeCell(identifier: PopularFilmCollectionViewCell.identifier, indexPath: indexPath) as! PopularFilmCollectionViewCell
-//        cell.data = data[indexPath.row].toMediaResult()
-//        return cell
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-//        let isLastItem = indexPath.row == data.count - 1
-//        let hasMorePage = currentPage < totalPages
-//        if (isLastItem && hasMorePage) {
-//            currentPage += 1
-//            startSearching(query: searchedQuery, page: currentPage)
-//        }
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        let selectedItemData = data[indexPath.row]
-//        if (selectedItemData.mediaType == "tv") {
-//            onTapMovie(id: selectedItemData.id ?? -1, contentType: .series)
-//        } else {
-//            onTapMovie(id: selectedItemData.id ?? -1, contentType: .movie)
-//        }
-//    }
-
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let itemWidth = ((collectionView.frame.width - 48) / 3).rounded(.down)
         let itemHeight = 250.0
